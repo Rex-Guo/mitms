b6ef11734bc16702714e8b364befbe7e2561fadc
neozhu <new163@163.com>
2018-06-29 10:28:58 +0800

    x

--- miTms/WebApp/App_Start/RouteConfig.cs
+++ miTms/WebApp/App_Start/RouteConfig.cs
@@ -17,7 +17,7 @@ namespace WebApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
             );
 
             
